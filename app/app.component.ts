4764d80e78c3be42cf51463e323db73fc52eaea9
Dennis Escueta <descueta@gmail.com>
2017-06-06 00:31:44 +1000

    my first commit

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'pm-app',
+    template:  `
+        <div><h1>{{pageTitle}}</h1>
+            <pm-products>My First Component</pm-products>
+        </div>
+    `    
+})
+export class AppComponent {
+    pageTitle: string = 'Acme Product Management';
+ 
+}
