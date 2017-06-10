833202afeac160b27f0796460e636e05d3989750
Dennis Escueta <descueta@gmail.com>
2017-06-10 22:55:04 +1000

    Up to Service and Dependency

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
+import { ProductService } from './products/product.service';
 @Component({
     selector: 'pm-app',
     template:  `
         <div><h1>{{pageTitle}}</h1>
             <pm-products>My First Component</pm-products>
         </div>
-    `    
+    `,
+    providers: [ ProductService ]    
 })
 export class AppComponent {
     pageTitle: string = 'Acme Product Management';
