69214cb491d0d1dce1a1d5ccb371b2910bd04fd6
Dennis Escueta <descueta@gmail.com>
2017-06-19 23:44:26 +1000

    Up to Navigation and Touting Additional Techniques

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -3,8 +3,7 @@ import { IProduct } from './product';
 import { ProductService } from './product.service';
 
 
-@Component({
-    selector: 'pm-products',
+@Component({    
     moduleId: module.id,
     templateUrl: 'product-list.component.html',
     styleUrls: ['product-list.component.css']
@@ -16,6 +15,7 @@ export class ProductListComponent implements OnInit {
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string;
+    errorMessage: string;
     products: IProduct[];
 
     constructor(private _productService: ProductService) {
@@ -27,7 +27,10 @@ export class ProductListComponent implements OnInit {
 
     ngOnInit(): void {
         console.log('In OnInit');
-        this.products = this._productService.getProducts();
+        this._productService.getProducts()
+            .subscribe(
+                products => this.products = products,
+                error => this.errorMessage = <any>error);
     }
 
     onRatingClicked(message: string): void {
